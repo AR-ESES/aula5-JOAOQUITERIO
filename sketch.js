f4529c5771d4c9968e6bbb0731f6e649635fe7b5
JOAOQUITERIO <102222007+JOAOQUITERIO@users.noreply.github.com>
2022-04-27 23:09:14 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,28 +1,29 @@
 let laranjas;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background(0)
+    createCanvas(windowWidth, windowHeight);
+    background(0);
 
 }
 
 function draw() {
 
-	background(0);
-	if(mouseY >= height/2) {
-		noFill();
+    background(0)
 
-	}
+    if(mouseY>=height/2 && mouseX<=width/2)
+    fill    (123,104,238); // azul
 
-	else {
-		fill(230);
+    else if(mouseY<height/2 && mouseX<=width/2) 
+    fill    (220,20,60); // vermelho
 
-	}
+    else if(mouseY<height/2 && mouseX>width/2) 
+    fill    (0,255,127); // verde 
 
-	stroke(230);
-	strokeWeight(2);
-	
-	circle(width / 2, height / 2, height / 4);
+    else{
+         noFill();
+    }
 
-
+    stroke(230);
+    strokeWeight(2);
+    circle(width / 2, height / 2, height / 4);
 }
